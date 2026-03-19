0ec139d731f30ae1cde2d04f223eaed21e615494
Bao Pham <louis.phammm@gmail.com>
2026-03-19 17:18:01 +0700

    Add secret for deployment

--- src/main/java/vn/com/routex/hub/booking/service/infrastructure/cache/redisson/config/RedissonConfig.java
+++ src/main/java/vn/com/routex/hub/booking/service/infrastructure/cache/redisson/config/RedissonConfig.java
@@ -3,17 +3,21 @@ package vn.com.routex.hub.booking.service.infrastructure.cache.redisson.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
 
-
+    @Value("${spring.data.redis.host}")
+    private String redisHost;
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://127.0.0.1:6379").setDatabase(0);
+
+        String redisAddress = String.format("redis://%s:6379", redisHost);
+        config.useSingleServer().setAddress(redisAddress).setDatabase(0);
 
         return Redisson.create(config);
     }
